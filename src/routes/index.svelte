7a0de0341ddd477aa0a18761c376155e8d1ffe46
Jacob Tucker <tsnakejake@aol.com>
2022-02-15 20:16:24 -0600

    add light/dark mode

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,23 @@
 
 <script>
 
+  import { onMount } from 'svelte'; 
+  import { theme } from '$lib/stores.js';
+
+  let toggleTheme;
+
+  onMount(() => {
+    let html = document.querySelector('html')
+    let currentTheme = html.getAttribute('data-theme');
+    $theme = currentTheme;
+    toggleTheme = () => {
+      let newTheme = $theme === 'light' ? 'dark' : 'light';
+      html.setAttribute('data-theme', newTheme);
+      $theme = newTheme;
+      localStorage.setItem('theme', newTheme);
+    }
+  })
+
 </script>
 
 <svelte:head>
@@ -11,13 +28,21 @@
   <div class="title">
     <h1>
       <span class="text">
-        <span>Emerald</span>
+        <span style="text-align: right;">Emerald</span>
         <img id="logo" src="/favicon.png" alt="emerald academy logo" />
         <span>Academy</span>
       </span>
-  </h1>
+    </h1>
     <span class="line"></span>
   </div>
+
+  <!-- Toggle theme -->
+  <a class="theme-toggle" href="/" on:click|preventDefault={toggleTheme}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-fill" viewBox="0 0 16 16">
+      <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
+    </svg>
+  </a>
+
   <article class="description">Learn everything about the Flow Blockchain and the Cadence Smart Contract programming language.</article>
   <div class="cards">
     <a href="https://discord.gg/emeraldcity" target="_blank">
@@ -38,6 +63,20 @@
 </div>
 
 <style>
+
+  .theme-toggle {
+    opacity: 0;
+    padding: 0;
+    height: 45px;
+    width: 45px;
+    background-color: var(--primary-focus);
+    border-radius: 50%;
+    display: inline-block;
+    text-align: center;
+    animation: show .5s forwards;
+    animation-delay: 1.5s;
+  }
+
   #logo {
     width: 70px;
     height: 70px;
@@ -128,7 +167,8 @@
     transform: translateY(100%);
     animation: comeBack 1s forwards;
     animation-delay: 0.75;
-    display: flex;
+    display: grid;
+    grid-template-columns: 1fr 70px 1fr;
     align-items: center;
   }
 
