8fde1e250b834ffc87e4a823b4e007a0ca7c78e9
jacob-tucker <jacobtucker818@gmail.com>
2022-07-15 13:06:24 -0400

    old recording for july 2022 2/8

--- src/lib/data/bootcamps.js
+++ src/lib/data/bootcamps.js
@@ -176,7 +176,7 @@ export const bootcamps = [
       },
       {
         src: 'https://www.youtube.com/embed/s1uh1AiXPmE',
-        title: 'Workshop #1',
+        title: 'Workshop 1/8',
         subject: [
           'What is the Flow Blockchain and Smart Contracts?',
           'Introduction to Programming [optional]',
@@ -266,6 +266,15 @@ export const bootcamps = [
           'Transactions, Scripts, and Cadence Basics',
         ],
         content: ['Chapter 1, Day 1', 'Chapter 1, Day 2', 'Chapter 2, Day 1', 'Chapter 2, Day 2'],
+      },
+      {
+        src: 'https://www.youtube.com/embed/66_2CWOPZLs',
+        title: 'Workshop 2/8',
+        subject: [
+          'Arrays, Dictionaries, and Optionals',
+          'Structs'
+        ],
+        content: ['Chapter 2, Day 3', 'Chapter 2, Day 4'],
       },
     ],
     description: 'Learn what the Flow blockchain is and how to start writing smart contracts in Cadence.',
